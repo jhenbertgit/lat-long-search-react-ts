a7139fb773c26fa41453401cca6fc138e01a4640
Jhenbert Villamucho <jvv205756@gmail.com>
2023-09-24 22:19:57 +0800

    refactor useState to useReducer

--- src/components/LatLongSearch.tsx
+++ src/components/LatLongSearch.tsx
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Col, Row, Spinner } from "reactstrap";
 import ShowResults from "./ShowResults";
 import Form from "./Form";
 
-interface Dataset {
+type Dataset = {
   lat: string;
   lng: string;
   address: string;
-}
+};
 
-function LatLongSearch() {
-  const [query, setQuery] = useState<string>("");
-  const [status, setStatus] = useState<string>("typing");
-  const [affirmative, setAffirmative] = useState<boolean>(false);
-  const [dataset, setDataset] = useState<Dataset>({
+type State = {
+  query: string;
+  affirmative: boolean;
+  status: string;
+  dataset: Dataset;
+};
+
+type Action =
+  | { type: "query"; payload: string }
+  | { type: "affirmative"; affirmative: boolean }
+  | { type: "status"; status: string }
+  | { type: "dataset"; dataset: Dataset };
+
+const initialState = {
+  query: "",
+  affirmative: false,
+  status: "typing",
+  dataset: {
     lat: "",
     lng: "",
     address: "",
-  });
+  },
+};
+
+function LatLongSearch() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const url = "https://trueway-geocoding.p.rapidapi.com";
 
@@ -30,18 +47,19 @@ function LatLongSearch() {
   };
 
   const onDismissed = () => {
-    setAffirmative(false);
+    dispatch({ type: "affirmative", affirmative: false });
   };
 
   const fetchResults = async (query: string) => {
-    setStatus("loading");
+    dispatch({ type: "status", status: "loading" });
+
+    if (query.trim() === "") {
+      dispatch({ type: "affirmative", affirmative: true });
+      dispatch({ type: "status", status: "typing" });
+      return;
+    }
+
     try {
-      //form validation
-      if (query === null || query.match(/^ *$/) !== null) {
-        setAffirmative(true);
-        setStatus("typing");
-        return;
-      }
       const response = await fetch(
         `${url}/Geocode?address=${query}&language=en`,
         options
@@ -49,8 +67,9 @@ function LatLongSearch() {
       const data = await response.json();
       const results = data.results[0] || {};
 
-      setDataset(
-        {
+      dispatch({
+        type: "dataset",
+        dataset: {
           lat: results.location.lat,
           lng: results.location.lng,
           address: results.address,
@@ -58,35 +77,39 @@ function LatLongSearch() {
           lat: "",
           lng: "",
           address: "",
-        }
-      );
-      setStatus("success");
+        },
+      });
+
+      dispatch({ type: "status", status: "success" });
     } catch (error) {
       console.error(error);
     }
   };
 
   const handleSearch = () => {
-    fetchResults(query);
+    fetchResults(state.query);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    setStatus("typing");
+    dispatch({
+      type: "query",
+      payload: event.target.value,
+    });
+    dispatch({ type: "status", status: "typing" });
   };
 
-  const isSuccess = status === "success";
-  const isLoading = status === "loading";
+  const isSuccess = state.status === "success";
+  const isLoading = state.status === "loading";
 
   return (
     <Row className="justify-content-center">
       <Col md={8} className="mt-5">
         <Form
-          alertIsOpen={affirmative}
+          alertIsOpen={state.affirmative}
           alertToggle={onDismissed}
-          query={query}
+          query={state.query}
           inputOnChange={handleChange}
-          btnDisabled={affirmative}
+          btnDisabled={state.affirmative}
           btnOnClick={handleSearch}
           isLoading={isLoading}
         />
@@ -100,14 +123,37 @@ function LatLongSearch() {
         )}
         {isSuccess && (
           <ShowResults
-            latitude={dataset.lat}
-            longitude={dataset.lng}
-            address={dataset.address}
+            latitude={state.dataset.lat}
+            longitude={state.dataset.lng}
+            address={state.dataset.address}
           />
         )}
       </Col>
     </Row>
   );
 }
-
 export default LatLongSearch;
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "query": {
+      return { ...state, query: action.payload };
+    }
+
+    case "affirmative": {
+      return { ...state, affirmative: action.affirmative };
+    }
+
+    case "status": {
+      return { ...state, status: action.status };
+    }
+
+    case "dataset": {
+      return { ...state, dataset: action.dataset };
+    }
+
+    default: {
+      throw new Error();
+    }
+  }
+};
