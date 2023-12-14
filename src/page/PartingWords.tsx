decd4c8f24aab0e0cb02a389c0fe1e952fc660df
Jhenbert Villamucho <jvv205756@gmail.com>
2023-12-14 18:08:44 +0800

    modified parting words

--- src/page/PartingWords.tsx
+++ src/page/PartingWords.tsx
@@ -1,19 +1,19 @@
 import { useFetch } from "@jhenbertnpm/use-fetch";
-import { Card, CardBody, CardText } from "reactstrap";
+import { Card, CardBody, CardText, Col, Spinner } from "reactstrap";
 
 const PartingWords = () => {
   const options = {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": import.meta.env.VITE_RAPIDAPI_KEY,
-      "X-RapidAPI-Host": "motivational-content.p.rapidapi.com",
+      "X-RapidAPI-Host": "thought-of-the-day.p.rapidapi.com",
     },
   };
 
-  const { data } = useFetch({
+  const { data, isLoaded } = useFetch({
     fetchFn: async () => {
       const response = await fetch(
-        "https://motivational-content.p.rapidapi.com/quotes/4",
+        "https://thought-of-the-day.p.rapidapi.com/thought",
         options
       );
       const result = await response.json();
@@ -24,20 +24,33 @@ const PartingWords = () => {
 
   return (
     <>
-      <div className="position-relative">
-        <img
-          src="https://picsum.photos/900"
-          alt="bg-image"
-          style={{ width: "100%", height: "100vh" }}
-        />
-      </div>
-      <div className="position-absolute top-50 start-50 translate-middle">
-        <Card className="shadow">
-          <CardBody>
-            <CardText className="fs-3 fst-italic">{data.quote}</CardText>
-          </CardBody>
-        </Card>
-      </div>
+      {isLoaded ? (
+        <>
+          <div className="position-relative">
+            <img
+              src="https://picsum.photos/900"
+              alt="bg-image"
+              style={{ width: "100%", height: "100vh" }}
+              loading="lazy"
+              className="object-fit-cover"
+            />
+          </div>
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <Card className="shadow">
+              <CardBody>
+                <CardText className="fs-3 fst-italic">{data.data}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </>
+      ) : (
+        <Col className="mt-4 d-flex justify-content-center">
+          <Spinner
+            color="primary"
+            style={{ height: "7rem", width: "7rem" }}
+          ></Spinner>
+        </Col>
+      )}
     </>
   );
 };
