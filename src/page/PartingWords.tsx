f7c2d5520171204a7b8c100c8b1e5a4919675b96
Jhenbert Villamucho <jvv205756@gmail.com>
2023-12-14 18:23:42 +0800

    minor commit

--- src/page/PartingWords.tsx
+++ src/page/PartingWords.tsx
@@ -1,5 +1,5 @@
 import { useFetch } from "@jhenbertnpm/use-fetch";
-import { Card, CardBody, CardText } from "reactstrap";
+import { Card, CardBody, CardText, Col, Spinner } from "reactstrap";
 
 const PartingWords = () => {
   const options = {
@@ -24,20 +24,32 @@ const PartingWords = () => {
 
   return (
     <>
-      <div className="position-relative">
-        <img
-          src="https://picsum.photos/900"
-          alt="bg-image"
-          style={{ width: "100%", height: "100vh" }}
-        />
-      </div>
-      <div className="position-absolute top-50 start-50 translate-middle">
-        <Card className="shadow">
-          <CardBody>
-            <CardText className="fs-3 fst-italic">{data.quote}</CardText>
-          </CardBody>
-        </Card>
-      </div>
+      {isLoaded ? (
+        <>
+          <div className="position-relative">
+            <img
+              src="https://picsum.photos/900"
+              alt="bg-image"
+              style={{ width: "100%", height: "100vh" }}
+              loading="lazy"
+            />
+          </div>
+          <div className="position-absolute top-50 start-50 translate-middle">
+            <Card className="shadow">
+              <CardBody>
+                <CardText className="fs-3 fst-italic">{data.data}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+        </>
+      ) : (
+        <Col className="mt-4 d-flex justify-content-center">
+          <Spinner
+            color="primary"
+            style={{ height: "7rem", width: "7rem" }}
+          ></Spinner>
+        </Col>
+      )}
     </>
   );
 };
